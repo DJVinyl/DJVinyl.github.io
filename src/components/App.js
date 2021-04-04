9dc51e1067b364e73353456dd4e2d02236000f88
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-04-04 16:38:45 -0600

    Initial Commit of Landing Page

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mitchell Gonzalez</h1>
+        <p>Software Developer</p>
+      </header>
+      {/* <a href="https://github.com/Joeyryanbridges">
+      <Image src="giticon.png" className="githubIcon" />
+      </a> */}
+    </div>
+  );
+}
+
+export default App;
