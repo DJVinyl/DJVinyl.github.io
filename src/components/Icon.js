2586e692b6c87ec5539f23fdc01b2383b7a77347
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-04-05 11:22:10 -0600

    removes gh-pages branch

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,7 +3,7 @@ import '../styles/Icon.scss';
 
 export default function Icon(props) {
   return (
-    <a className='dyn-img' href= {props.website}>
+    <a className='dyn-img' href= {props.website} target="_blank">
     <Image src={props.image} className={`${props.image} + Icon`}/>
     </a>
   )
