a44699f8dedfb24d53cfeb0a8de5989a5df8a130
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-04-30 11:18:18 -0600

    added mroe css

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,12 +2,14 @@ import '../styles/About.scss'
 
 export default function About() {
   return (
-    <div>
-      <p className="about-me">
-        Welcome to my portfolio website! A website that can redirect you to my other important websites, and displays my coding portfolio.
+    <div className='about-me'>
+      <p className="about-me-description">
+        <h2>Welcome to my portfolio website!</h2>
+        A website with my personal link tree and displays my coding portfolio.
         Feel free to fork this website from my github to use for your own portfolio website. This was built with ReactJS-Typescript.
-        Also feel free to try and find the easter egg. 
       </p>
+      <br></br>
+      <hr></hr>
     </div>
   );
 }
