6336ebc0b106e17ad34ea8ff39cf03a114820454
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-04-30 21:53:17 -0600

    added animations to page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,20 @@
 import Header from './Header'
 import About from './About'
 import Portfolio from './Portfolio'
+import FadeIn from 'react-fade-in';
 import '../styles/App.scss'
 
 export default function App() {
   return (
     <div className="App">
-      <Header />
+      <FadeIn delay={300}>
+        <Header />
+      </FadeIn>
       <div className="content-container">
-        <About />
-        <Portfolio />
+        <FadeIn delay={600}>
+          <About />
+          <Portfolio />
+        </FadeIn>
       </div>
     </div>
   );
