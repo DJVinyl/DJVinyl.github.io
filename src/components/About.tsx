ec7985bcb9759b0692df0226e6393109e9563de1
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-04-30 22:03:42 -0600

    ran lighthouse report

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,12 +2,13 @@ import '../styles/About.scss'
 
 export default function About() {
   return (
-    <div className='about-me'>
+    <div className="about-me">
+      <h2>Welcome to my portfolio website!</h2>
       <p className="about-me-description">
-        <h2>Welcome to my portfolio website!</h2>
-        A website that shows my personal link tree and displays my coding portfolio. 
-        A website I hope to evolve alongside of my skillset.
-        Feel free to fork the code from my Github to use for your own portfolio website. This was built with ReactJS-Typescript.
+        A website that shows my personal link tree and displays my coding
+        portfolio. A website I hope to evolve alongside of my skillset. Feel
+        free to fork the code from my Github to use for your own portfolio
+        website. This was built with ReactJS-Typescript.
       </p>
       <br></br>
       <hr></hr>
